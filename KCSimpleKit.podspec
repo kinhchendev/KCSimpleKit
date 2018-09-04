951d9280edded29c3e1251ad8779a8471500ee7b
Tran Vinh Kinh <tranvinhkinh@Trans-iMac.local>
2018-09-04 14:21:27 +0700

    update version 0.0.7

--- KCSimpleKit.podspec
+++ KCSimpleKit.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "KCSimpleKit"
-  s.version      = "0.0.6"
+  s.version      = "0.0.7"
   s.summary      = "Swift framework with Macro and Extension"
   s.swift_version = "4.0"
   s.platform     = :ios, "9.0"
--- KCSimpleKit/Source/Macro/Macro.swift
+++ KCSimpleKit/Source/Macro/Macro.swift
@@ -19,18 +19,18 @@ import UIKit
     }
     
 // MARK: Color
-    public static func rgb(r : Int, g : Int, b : Int, alpha : CGFloat) -> UIColor {
+    @objc public static func rgb(r : Int, g : Int, b : Int, alpha : CGFloat) -> UIColor {
         return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: alpha)
     }
     
-    public static func colorFromHex(hexValue hex : Int) -> UIColor {
+    @objc public static func colorFromHex(hexValue hex : Int) -> UIColor {
         return UIColor(red: CGFloat((hex >> 16) & 0xFF) / 255.0,
                        green: CGFloat((hex >> 8) & 0xFF) / 255.0,
                        blue: CGFloat(hex & 0xFF) / 255.0,
                        alpha: 1.0)
     }
     
-    public static func colorFromHex(hexValue hex : Int, WithAlpha alpha : CGFloat) -> UIColor {
+    @objc public static func colorFromHex(hexValue hex : Int, WithAlpha alpha : CGFloat) -> UIColor {
         return UIColor(red: CGFloat((hex >> 16) & 0xFF) / 255.0,
                        green: CGFloat((hex >> 8) & 0xFF) / 255.0,
                        blue: CGFloat(hex & 0xFF) / 255.0,
@@ -38,14 +38,14 @@ import UIKit
     }
     
 // MARK: Window
-    public static let mainWindow : UIWindow? = UIApplication.shared.windows.first
+    @objc public static let mainWindow : UIWindow? = UIApplication.shared.windows.first
     
 // MARK: Device
-    public static let deviceIsIphone = (UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone)
-    public static let deviceIsIpad = (UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.pad)
+    @objc public static let deviceIsIphone = (UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone)
+    @objc public static let deviceIsIpad = (UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.pad)
     
 // MARK: Resolution
-    public static let isRetina = (UIScreen.main.responds(to: #selector(UIScreen.displayLink(withTarget:selector:))) && UIScreen.main.scale == 2)
+    @objc public static let isRetina = (UIScreen.main.responds(to: #selector(UIScreen.displayLink(withTarget:selector:))) && UIScreen.main.scale == 2)
     
     public static func imageExtensionForRetina() -> String {
         if Macro.isRetina {
@@ -54,25 +54,25 @@ import UIKit
         return ""
     }
     
-    public static let isIphone_4_protrait : Bool = Double(fabs(UIScreen.main.bounds.size.height - 480)) < Double.ulpOfOne
-    public static let isIphone_4_landscape : Bool = Double(fabs(UIScreen.main.bounds.size.width - 480)) < Double.ulpOfOne
-    public static let isIphone_5_protrait : Bool = Double(fabs(UIScreen.main.bounds.size.height - 568)) < Double.ulpOfOne
-    public static let isIphone_5_landscape : Bool = Double(fabs(UIScreen.main.bounds.size.width - 568)) < Double.ulpOfOne
-    public static let isIphone_6_protrait : Bool = Double(fabs(UIScreen.main.bounds.size.height - 667)) < Double.ulpOfOne
-    public static let isIphone_6_landscape : Bool = Double(fabs(UIScreen.main.bounds.size.width - 667)) < Double.ulpOfOne
-    public static let isIphone_6plus_protrait : Bool = Double(fabs(UIScreen.main.bounds.size.height - 736)) < Double.ulpOfOne
-    public static let isIphone_6plus_landscape : Bool = Double(fabs(UIScreen.main.bounds.size.width - 736)) < Double.ulpOfOne
-    public static let isIphone_X_protrait : Bool = Double(fabs(UIScreen.main.bounds.size.height - 812)) < Double.ulpOfOne
-    public static let isIphone_X_landscape : Bool = Double(fabs(UIScreen.main.bounds.size.width - 812)) < Double.ulpOfOne
+    @objc public static let isIphone_4_protrait : Bool = Double(fabs(UIScreen.main.bounds.size.height - 480)) < Double.ulpOfOne
+    @objc public static let isIphone_4_landscape : Bool = Double(fabs(UIScreen.main.bounds.size.width - 480)) < Double.ulpOfOne
+    @objc public static let isIphone_5_protrait : Bool = Double(fabs(UIScreen.main.bounds.size.height - 568)) < Double.ulpOfOne
+    @objc public static let isIphone_5_landscape : Bool = Double(fabs(UIScreen.main.bounds.size.width - 568)) < Double.ulpOfOne
+    @objc public static let isIphone_6_protrait : Bool = Double(fabs(UIScreen.main.bounds.size.height - 667)) < Double.ulpOfOne
+    @objc public static let isIphone_6_landscape : Bool = Double(fabs(UIScreen.main.bounds.size.width - 667)) < Double.ulpOfOne
+    @objc public static let isIphone_6plus_protrait : Bool = Double(fabs(UIScreen.main.bounds.size.height - 736)) < Double.ulpOfOne
+    @objc public static let isIphone_6plus_landscape : Bool = Double(fabs(UIScreen.main.bounds.size.width - 736)) < Double.ulpOfOne
+    @objc public static let isIphone_X_protrait : Bool = Double(fabs(UIScreen.main.bounds.size.height - 812)) < Double.ulpOfOne
+    @objc public static let isIphone_X_landscape : Bool = Double(fabs(UIScreen.main.bounds.size.width - 812)) < Double.ulpOfOne
     
-    public static func DDV(ipadValue : CGFloat, iphoneValue : CGFloat) -> CGFloat {
+    @objc public static func DDV(ipadValue : CGFloat, iphoneValue : CGFloat) -> CGFloat {
         if Macro.deviceIsIpad {
             return ipadValue
         }
         return iphoneValue
     }
     
-    public static func DDDV(ipadValue : CGFloat,
+    @objc public static func DDDV(ipadValue : CGFloat,
                             iphone_X_value : CGFloat,
                             iphone_6plus_value : CGFloat,
                             iphone_6_value : CGFloat,
@@ -97,49 +97,49 @@ import UIKit
     }
     
 // MARK: System version
-    public static func osVersionEqualTo(version v : String) -> Bool {
+    @objc public static func osVersionEqualTo(version v : String) -> Bool {
         let osversion = UIDevice.current.systemVersion
         
         return osversion.compare(v, options: String.CompareOptions.numeric, range: nil, locale: nil) == ComparisonResult.orderedSame
     }
     
-    public static func osVersionGreaterThan(version v : String) -> Bool {
+    @objc public static func osVersionGreaterThan(version v : String) -> Bool {
         let osversion = UIDevice.current.systemVersion
         
         return osversion.compare(v, options: String.CompareOptions.numeric, range: nil, locale: nil) == ComparisonResult.orderedDescending
     }
     
-    public static func osVersionGreaterThanOrEqualTo(version v : String) -> Bool {
+    @objc public static func osVersionGreaterThanOrEqualTo(version v : String) -> Bool {
         let osversion = UIDevice.current.systemVersion
         
         return osversion.compare(v, options: String.CompareOptions.numeric, range: nil, locale: nil) != ComparisonResult.orderedAscending
     }
     
-    public static func osVersionLessThan(version v : String) -> Bool {
+    @objc public static func osVersionLessThan(version v : String) -> Bool {
         let osversion = UIDevice.current.systemVersion
         
         return osversion.compare(v, options: String.CompareOptions.numeric, range: nil, locale: nil) == ComparisonResult.orderedAscending
     }
     
-    public static func osVersionLessThanOrEqualTo(version v : String) -> Bool {
+    @objc public static func osVersionLessThanOrEqualTo(version v : String) -> Bool {
         let osversion = UIDevice.current.systemVersion
         
         return osversion.compare(v, options: String.CompareOptions.numeric, range: nil, locale: nil) != ComparisonResult.orderedDescending
     }
     
 // MARK: User Default
-    public static func saveToUserDefault(obj : AnyObject, key : String) -> Void {
+    @objc public static func saveToUserDefault(obj : AnyObject, key : String) -> Void {
         let ud : UserDefaults = UserDefaults.standard
         ud.set(obj, forKey: key)
         ud.synchronize()
     }
     
-    public static func getFromUserDefault(key : String) -> AnyObject? {
+    @objc public static func getFromUserDefault(key : String) -> AnyObject? {
         let ud : UserDefaults = UserDefaults.standard
         return ud.object(forKey: key) as AnyObject?
     }
     
-    public static func removeFromUserDefault(key : String) -> Void {
+    @objc public static func removeFromUserDefault(key : String) -> Void {
         let ud : UserDefaults = UserDefaults.standard
         ud.removeObject(forKey: key)
         ud.synchronize()
@@ -152,7 +152,7 @@ import UIKit
         })
     }
     
-    public static func dispatchOnBackgroundThreadAfter(afterSecond time : TimeInterval, execute : @escaping ()->(Void)) {
+    @objc public static func dispatchOnBackgroundThreadAfter(afterSecond time : TimeInterval, execute : @escaping ()->(Void)) {
         DispatchQueue.main.asyncAfter(deadline: .now() + time, execute: {
             DispatchQueue.global(qos: .background).async {
                 print("This is run on the background queue")
